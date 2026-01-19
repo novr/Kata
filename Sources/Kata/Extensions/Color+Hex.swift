b4c3daccdae48464134425232f9d4e53b2fa1a94
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2026-01-19 09:54:48 +0900

    fix: lint for identifier_name

--- Sources/Kata/Extensions/Color+Hex.swift
+++ Sources/Kata/Extensions/Color+Hex.swift
@@ -5,7 +5,7 @@ extension Color {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
         Scanner(string: hex).scanHexInt64(&int)
-        
+
         let a, r, g, b: UInt64
         switch hex.count {
         case 3: // RGB (12-bit)
@@ -17,7 +17,7 @@ extension Color {
         default:
             (a, r, g, b) = (255, 0, 0, 0)
         }
-        
+
         self.init(
             .sRGB,
             red: Double(r) / 255,
